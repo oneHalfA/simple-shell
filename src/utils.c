1dff7aac6f6969fc1e5e69a14dbbffe6e30eaaf3
oneHalfA <nimaeraghi98@gmail.com>
2025-05-27 08:29:32 +0330

    Added free_buffer function to free path or buffer based on `type`

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,4 @@
 #include "utils.h"
-#include <stdarg.h>
 
 void print_log(const char *f, ...) {
   va_list args;
@@ -8,4 +7,27 @@ void print_log(const char *f, ...) {
   vprintf(f, args);
   printf("\033[0m\n");
   va_end(args);
+}
+
+void free_buffer(tokenizedFormat *t, char type) {
+  uint8_t len = 0;
+  switch (type) {
+  case 'p': // for path
+    len = t->argn;
+    break;
+  case 'b': // for buffer
+    len = t->argn + 1;  // +1 because execv function needs to have null terminated argv
+    break;
+  default:
+    LOG("WRONG TYPE `%c` TO FREE MEMORY", type);
+    exit(1);
+    break;
+  }
+
+  for (int i = 0; i < len; i++) {
+    free(t->argv[i]);
+    t->argv[i] = NULL;
+  }
+  free(t->argv);
+  t->argv = NULL;
 }
