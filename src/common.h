d7da06ed143602160dfa3a4072b2ebebc7df34de
oneHalfA <nimaeraghi98@gmail.com>
2025-05-27 07:48:07 +0330

    Add common.h header with core data structures and includes
    - Define inputBuffer and tokenizedFormat structs for command parsing
    - Define generic linked list struct
    - Include essential standard headers for shell functionality
    - Declare static path variable for environment path handling

---
+++ src/common.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h> // chdir is here
+#include <string.h>
+#include <stdlib.h> // getenv is here
+
+typedef struct {
+  char **argv;
+  uint8_t argn;
+} tokenizedFormat;
+
+typedef struct {
+  char *input;
+  uint8_t input_len;
+  bool validation;
+  tokenizedFormat tc;
+} inputBuffer;
+
+typedef struct list {
+  struct list *next;
+  void *data;
+} list;
+
+static const char *path = NULL;
+
+#endif
